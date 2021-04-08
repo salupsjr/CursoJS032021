7dd5529dea309507b90b7ed8a1ba8b9085aed932
salupsjr <salupsjuninho@gmail.com>
2021-04-08 10:49:51 -0300

    Arrays e seus métodos

---
+++ ExemplosJS/arrays.js
@@ -0,0 +1,30 @@
+const frutas = ['Banana','Laranja','Mamão', 'Acerola']
+//const frutasLength = frutas.push('Acerola')
+
+//console.log(frutas);
+//console.log(frutasLength);
+
+//const tiraFruta = frutas.p/op() //tira o ultimo item/indice do array frutas
+
+//const colocaPrimeiraFruta = frutas.unshift('Pera') //Insere um item/indice na primeira posição do array;
+
+//const organizaFrutas = frutas.sort()
+//const tiraPrimeiraFruta = frutas.shift()
+
+//console.log(`Fruta removida: ${tiraPrimeiraFruta}`);
+
+//const alimentos = frutas.concat(salgados)
+//const salgados = ['Empada','Coxinha','Kibe']
+//console.log(alimentos);
+
+//console.log(frutas.slice(-2))//Mostra as duas ultima posições do array
+
+//console.log(frutas.splice(1, 0, 'Pera'))
+
+//console.log(frutas);
+
+//console.log(frutas.splice(3, 1, 'Uva', 'Kiwi'))
+
+//console.log(frutas);
+
+frutas.forEach(fruta => console.log(fruta));
